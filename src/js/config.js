f0243e3982172146f6ed3d2a3e5e4e906c6f2a93
Stefan Radenkovic <stefanthadeus@gmail.com>
2026-03-13 12:40:17 +0100

    Implement recipe upload

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = "5b3c88a9-1e7c-4d8b-9f0e-1a2b8c9d0e1f";
+export const MODAL_CLOSE_SEC = 2.5;
